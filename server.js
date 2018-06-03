d94b28875096ec70f50880c50e5e966b4d8aeb98
yuning1207 <1578517699@qq.com>
2018-06-03 18:38:20 +0800

    修改

--- server.js
+++ server.js
@@ -1,63 +1,63 @@
-// import { stat } from 'fs/promises';
+import { stat } from 'fs/promises';
 
-// var fs = require('fs');
-// var path = require('path');
-// var http = require('http');
-// var MIME = {
-//     '.css': 'text/css',
-//     '.js': 'application/javascript'
-// };
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var MIME = {
+    '.css': 'text/css',
+    '.js': 'application/javascript'
+};
 
-// function parseurl(root, url) {
-//     if (url.indexof('??') === -1) {
-//         url = url.replace('/', '/??');
-//     }
-//     var path = url.split('??');
-//     var pathnames = path[1].split(',').map((value) => path.join(root, path[0], value));
-//     return {
-//         mime: MIME[path.extname(pathnames[0])] || 'text/plain',
-//         pathnames: pathnames
-//     }
-// }
+function parseurl(root, url) {
+    if (url.indexof('??') === -1) {
+        url = url.replace('/', '/??');
+    }
+    var path = url.split('??');
+    var pathnames = path[1].split(',').map((value) => path.join(root, path[0], value));
+    return {
+        mime: MIME[path.extname(pathnames[0])] || 'text/plain',
+        pathnames: pathnames
+    }
+}
 
-// function combine(pathnames, callback) {
-//     var output = [];
-//     (function next(i, len) {
-//         if (i < len) {
-//             fs.readFile(pathnames[i], function(err, data) {
-//                 if (err) {
-//                     callback(err);
-//                 } else {
-//                     output.push(data);
-//                     next(i + 1, len);
-//                 }
-//             })
-//         } else {
-//             callback(null, Buffer.concat(output));
-//         }
-//     }(0, pathnames.length))
-// }
+function combine(pathnames, callback) {
+    var output = [];
+    (function next(i, len) {
+        if (i < len) {
+            fs.readFile(pathnames[i], function(err, data) {
+                if (err) {
+                    callback(err);
+                } else {
+                    output.push(data);
+                    next(i + 1, len);
+                }
+            })
+        } else {
+            callback(null, Buffer.concat(output));
+        }
+    }(0, pathnames.length))
+}
 
-// function main(argv) {
-//     var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8'));
-//     var root = config.root || '.';
-//     var port = config.port || 80;
-//     http.createServer(function(request, response) {
-//         var urlinfo = parseurl(root, request.url);
-//         combine(urlinfo.pathnames, function(err, data) {
-//             if (err) {
-//                 response.writeHead(404);
-//                 response.end(err.message);
-//             } else {
-//                 response.writeHead(200, {
-//                     'Content-Type': urlinfo.mime
-//                 });
-//                 response.end(data);
-//             }
-//         })
-//     }).listen(port);
-// }
-// main(process.argv.slice(2));
+function main(argv) {
+    var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8'));
+    var root = config.root || '.';
+    var port = config.port || 80;
+    http.createServer(function(request, response) {
+        var urlinfo = parseurl(root, request.url);
+        combine(urlinfo.pathnames, function(err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end(err.message);
+            } else {
+                response.writeHead(200, {
+                    'Content-Type': urlinfo.mime
+                });
+                response.end(data);
+            }
+        })
+    }).listen(port);
+}
+main(process.argv.slice(2));
 
 
 var fs = require('fs');
